9f7034594784916acda1c0e603edf142f0962261
Celoma <salouet.clement049@gmail.com>
2023-05-11 10:13:39 +0200

    .

--- js/login_signup.js
+++ js/login_signup.js
@@ -1,14 +1,20 @@
 const connexion = document.getElementById("headerConnexion");
 const inscription = document.getElementById("headerInscription");
-const body = document.querySelector("body");
+const body = document.querySelector(".Main");
 
 connexion.addEventListener('click', connexionPopUp);
 inscription.addEventListener('click', inscriptionPopUp);
 
 function connexionPopUp(){
     body.style.filter = 'blur(10px)';
+    document.querySelector(".PopUpBg").style.visibility = 'Visible';
+    document.querySelector(".PopUp").style.visibility = 'Visible';
+
+
 }
 
 function inscriptionPopUp(){
     body.style.filter = 'blur(10px)';
+    document.querySelector(".PopUpBg").style.visibility = 'Visible';
+    document.querySelector(".PopUp").style.visibility = 'Visible';
 }
