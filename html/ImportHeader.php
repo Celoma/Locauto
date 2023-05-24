ff1ef2747b0a32811493310f1e4e98ef5400023a
Celoma <salouet.clement049@gmail.com>
2023-05-24 22:40:41 +0200

    ee

--- html/ImportHeader.php
+++ html/ImportHeader.php
@@ -1,8 +1,22 @@
 <header>
     <div class='headerGauche'>
-        <img src='' class='navBarOpen'>
-        <div class='navBar'>
-        </div>
+        <nav class="nav">
+            <input type="checkbox" id="check">
+            <label for="check">
+                <i class="fas fa-bars" id="btn"></i>
+                <i class="fas fa-times" id="cancel"></i>
+            </label>
+            <div class="sidebar">
+                <strong class="info">Informations</strong>
+                <ul>
+                    <li><a href="../pages/index.html"><i class="fas fa-home"></i> Acceuil</a></li>
+                    <li><a href="#Apropos"><i class="fas fa-question-circle"></i> A propos de nous</a></li>
+                    <li><a href="#"><i class="fas fa-calendar-week"></i> Nos événements</a></li>
+                    <li><a href="#"><i class="fas fa-sliders-h"></i> Paramètres</a></li>
+                    <li><a href="#contacter""><i class="fas fa-envelope"></i> Nous contactez</a></li>
+                </ul>
+            </div>
+        </nav>
     </div>
     <div class='headerCentre'>
     <h1>Loc'Auto</h1>
@@ -12,3 +26,4 @@
         <?php include "header.php"?>
     </div>
 </header>
+<script src="https://kit.fontawesome.com/7628988a57.js" crossorigin="anonymous"></script>
--- html/index.php
+++ html/index.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="icon" href="../img/logo_blanc_noir.png">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <script src="https://kit.fontawesome.com/7628988a57.js" crossorigin="anonymous"></script>
+
 </head>
     <title>Loc'Auto</title>
 <body>
@@ -22,23 +22,6 @@
         <?php include "ImportHeader.php"?>
         <div class='BodyPage'>
     </div>
-    <nav class="nav">
-        <input type="checkbox" id="check">
-        <label for="check">
-            <i class="fas fa-bars" id="btn"></i>
-            <i class="fas fa-times" id="cancel"></i>
-        </label>
-        <div class="sidebar">
-            <header class="info"><strong>Informations</strong></header>
-            <ul>
-                <li><a href="../pages/index.html"><i class="fas fa-home"></i> Acceuil</a></li>
-                <li><a href="#Apropos"><i class="fas fa-question-circle"></i> A propos de nous</a></li>
-                <li><a href="#"><i class="fas fa-calendar-week"></i> Nos événements</a></li>
-                <li><a href="#"><i class="fas fa-sliders-h"></i> Paramètres</a></li>
-                <li><a href="#contacter""><i class="fas fa-envelope"></i> Nous contactez</a></li>
-            </ul>
-        </div>
-    </nav>
         <footer>
         </footer>
 
