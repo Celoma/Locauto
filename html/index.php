7bf52d553ae9d1f201702a22532ee6f32a0342cb
FlorianThl <florianthiel2312@gmail.com>
2023-06-05 09:50:25 +0200

    beug

--- html/index.php
+++ html/index.php
@@ -18,6 +18,26 @@
     <div class='Main'>
         <?php include "ImportHeader.php"?>
     </div>
+    <section class="home" id="home">
+        <div class="home-text">
+            <h1 class="eslogan"><strong>Votre Location <br><span>Avec</span> <br>Simplicité</strong></h1>
+            <p>Bienvenue chez Loc'Auto votre<br> spécialiste de la location de véhicule</p>
+            <a href="#" class="btn">Choisi ta voiture</a>
+        </div>
+    </section>
+    <section class="infos">
+        <section class="infos-text">
+            <h1 class="exp"><strong><span>15</span> ANS<br> D'EXPERIENCE</strong></h1>
+        </section>
+        <section class="right-infos-text">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.<br> Dolore iste in libero totam impedit soluta ad a natus<br> incidunt, at blanditiis alias magni, doloribus nemo,<br> eligendi quaerat laudantium modi nobis?</p>
+        </section>
+    </section>
+    <section class="reserv">
+        <div class="reserv-text">
+            <h1>Trouve ta <br>réservation</h1>
+        </div>
+    </section>
 	<footer>
     <div class="sm-handle">
                     <a href="#" class="sm-button"><i class="fab fa-instagram"></i></a>
