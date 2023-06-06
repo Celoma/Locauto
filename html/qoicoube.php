99e35bf3ecdd041adca86a219f29d011e2b8b40f
FlorianThl <florianthiel2312@gmail.com>
2023-06-06 10:56:23 +0200

    test

---
+++ html/qoicoube.php
@@ -0,0 +1,29 @@
+<?php
+    $id_session = session_id();
+    include "../bdd/biblio.php";
+    $connexion = connexion();
+    include "formTraitment.php"
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="../img/logo_blanc_noir.png">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <title>Loc'Auto</title>
+<body>
+    <!-- Remettre les valeurs des formulaires s'ils sont mal renseignés -->
+    <?php include "PopUpTraitment.php" ?>
+    <div class='Main'>
+        <?php include "ImportHeader.php"?>
+    </div>
+    <div class="quoiiii">
+    <h1 class="baka">QUOICOUBAKA</h1>
+    </div>
+    <?php include "footer.php" ?>
+
+<!-- Scripts -->
+<script type='text/javascript' src='../js/pageConnexion.js'></script>
+</body>
+</html>
